5679427237890184e35f5470a175a2beb5e5fc22
Holden Hinkle <holden@holdenhinkle.com>
2022-05-20 11:18:11 -0400

    rename requestVerifyResendScreent files, route and class

--- lib/src/features/authentication/presentation/request_verify_account_resend_screen.dart
+++ lib/src/features/authentication/presentation/request_verify_account_resend_screen.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:sorcery_desktop_v3/src/features/authentication/presentation/verify_account_resend_form.dart';
 
-class RequestVerifyResendScreen extends StatelessWidget {
-  const RequestVerifyResendScreen({Key? key}) : super(key: key);
+class RequestVerifyAccountResendScreen extends StatelessWidget {
+  const RequestVerifyAccountResendScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const _RequestVerifyResend();
+    return const _RequestVerifyAccountResend();
   }
 }
 
-class _RequestVerifyResend extends StatelessWidget {
-  const _RequestVerifyResend({Key? key}) : super(key: key);
+class _RequestVerifyAccountResend extends StatelessWidget {
+  const _RequestVerifyAccountResend({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const SizedBox box = SizedBox(height: 20);
-
     return Scaffold(
       appBar: AppBar(title: const Text('Sorcery')),
       body: Center(
         child: Column(
           children: const [
-            box,
-            Text('Resend Verify Account Email'),
-            box,
             VerifyAccountResendForm(),
           ],
         ),
