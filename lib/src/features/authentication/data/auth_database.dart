0904e7b68bf09e8ebcdb701528bd4c14bedc61ce
Holden Hinkle <holden@holdenhinkle.com>
2022-05-23 18:00:25 -0400

    move Hive init and registering Hive adapters to main

--- lib/src/features/authentication/data/auth_database.dart
+++ lib/src/features/authentication/data/auth_database.dart
@@ -2,15 +2,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sorcery_desktop_v3/src/features/authentication/domain/user.dart';
 
 class AuthDatabase {
-  AuthDatabase();
-
   Future<void> saveUser({required User user}) async {
-    // needs to be done once...
-    await Hive.initFlutter();
-
-    // needs to be done once
-    Hive.registerAdapter(UserAdapter());
-
     await _saveUser(user: user);
   }
 
