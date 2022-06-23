a49be91ae3df2f1d32360cc754850e06dc23a98c
Holden Hinkle <holden@holdenhinkle.com>
2022-06-22 20:57:47 -0400

    rename SignUpState AuthState

--- lib/src/features/authentication/presentation/auth_state.dart
+++ lib/src/features/authentication/presentation/auth_state.dart
@@ -1,30 +1,30 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SignUpState {
-  SignUpState({
+class AuthState {
+  AuthState({
     this.value = const AsyncValue.data(null),
   });
   final AsyncValue<void> value;
 
   bool get isLoading => value.isLoading;
 
-  SignUpState copyWith({
+  AuthState copyWith({
     AsyncValue<void>? value,
   }) {
-    return SignUpState(
+    return AuthState(
       value: value ?? this.value,
     );
   }
 
   @override
-  String toString() => 'SignUpState(value: $value)';
+  String toString() => 'AuthState(value: $value)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is SignUpState && other.value == value;
+    return other is AuthState && other.value == value;
   }
 
   @override
