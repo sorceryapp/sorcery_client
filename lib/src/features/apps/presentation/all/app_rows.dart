a515f68c1e51c3440e3a42069900b306b8b85906
Holden Hinkle <holden@holdenhinkle.com>
2022-06-25 15:27:16 -0400

    app name links to app by id

--- lib/src/features/apps/presentation/all/app_rows.dart
+++ lib/src/features/apps/presentation/all/app_rows.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
 
 class TableRows {
   final Iterable rowData;
@@ -10,8 +11,25 @@ class TableRows {
     rowData.toList().forEach((row) {
       List<DataCell> cells = [];
 
-      row.values.toList().forEach((value) {
-        cells.add(DataCell(Text(value.toString())));
+      row.forEach((key, value) {
+        if (key == 'name') {
+          cells.add(
+            DataCell(
+              Link(
+                uri: Uri.parse('/apps/${row['appId']}'),
+                builder: (context, followLink) => TextButton(
+                  onPressed: followLink,
+                  child: Text(
+                    value,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ),
+              ),
+            ),
+          );
+        } else {
+          cells.add(DataCell(Text(value.toString())));
+        }
       });
 
       rows.add(DataRow(cells: cells));
