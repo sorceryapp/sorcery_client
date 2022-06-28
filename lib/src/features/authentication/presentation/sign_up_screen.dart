1bc8af4163e55b88a3638e657be83fb67eab168c
Holden Hinkle <holden@holdenhinkle.com>
2022-06-28 17:51:12 -0400

    create new sign_up form

--- lib/src/features/authentication/presentation/sign_up_screen.dart
+++ lib/src/features/authentication/presentation/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sorcery_desktop_v3/localization/l10n.dart';
+import 'package:sorcery_desktop_v3/src/features/authentication/presentation/sign_up_form.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -28,6 +29,7 @@ class _SignUp extends StatelessWidget {
               style: const TextStyle(fontSize: 20),
             ),
             box,
+            const SignUpForm(),
           ],
         ),
       ),
