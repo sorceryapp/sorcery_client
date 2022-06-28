201dd6b3faa7d488fa2fec75c0f4cb8b2b6b5cb6
Holden Hinkle <holden@holdenhinkle.com>
2022-06-28 16:12:44 -0400

    remove old form builder code so app can run

--- lib/src/features/apps/presentation/create/apps_create_screen.dart
+++ lib/src/features/apps/presentation/create/apps_create_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 // import 'package:sorcery_desktop_v3/localization/l10n.dart';
-import 'package:sorcery_desktop_v3/src/form_builder/form_builder.dart';
-import 'package:sorcery_desktop_v3/src/form_builder/form_config.dart';
 
 class AppsCreateScreen extends StatelessWidget {
   const AppsCreateScreen({Key? key}) : super(key: key);
@@ -14,8 +12,6 @@ class AppsCreateScreen extends StatelessWidget {
 
 class CreateApp extends StatelessWidget {
   const CreateApp({Key? key}) : super(key: key);
-  final String _directoryName = 'apps';
-  final String _formFileName = 'create_app.yaml';
 
   @override
   Widget build(BuildContext context) {
@@ -33,19 +29,6 @@ class CreateApp extends StatelessWidget {
               style: const TextStyle(fontSize: 20),
             ),
             box,
-            FutureBuilder<Map>(
-              future: FormConfig().getFormConfig(
-                  context: context,
-                  directoryName: _directoryName,
-                  formFileName: _formFileName),
-              builder: (context, AsyncSnapshot<Map> snapshot) {
-                if (snapshot.hasData) {
-                  return FormBuilder(blueprint: snapshot.data as Map);
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
           ],
         ),
       ),
--- lib/src/features/authentication/presentation/reset_password_screen.dart
+++ lib/src/features/authentication/presentation/reset_password_screen.dart
@@ -1,6 +1,4 @@
 import 'package:sorcery_desktop_v3/localization/l10n.dart';
-import 'package:sorcery_desktop_v3/src/form_builder/form_builder.dart';
-import 'package:sorcery_desktop_v3/src/form_builder/form_config.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,9 +11,6 @@ class ResetPasswordScreen extends ConsumerStatefulWidget {
 }
 
 class _ResetPasswordScreen extends ConsumerState<ResetPasswordScreen> {
-  final String _directoryName = 'authentication';
-  final String _formFileName = 'reset_password.yaml';
-
   @override
   Widget build(BuildContext context) {
     const SizedBox box = SizedBox(height: 40);
@@ -29,19 +24,6 @@ class _ResetPasswordScreen extends ConsumerState<ResetPasswordScreen> {
             Text(
               SorceryLocalizations.current.resetPasswordPageTitle,
               style: const TextStyle(fontSize: 20),
-            ),
-            FutureBuilder<Map>(
-              future: FormConfig().getFormConfig(
-                  context: context,
-                  directoryName: _directoryName,
-                  formFileName: _formFileName),
-              builder: (context, AsyncSnapshot<Map> snapshot) {
-                if (snapshot.hasData) {
-                  return FormBuilder(blueprint: snapshot.data as Map);
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
             ),
           ],
         ),
