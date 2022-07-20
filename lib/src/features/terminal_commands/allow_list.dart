25ff4e1df3e83c64a0c8ee8292f0a9b821df6eb1
Holden Hinkle <holden@holdenhinkle.com>
2022-07-20 14:52:00 -0400

    create allow list of user-entered commands

---
+++ lib/src/features/terminal_commands/allow_list.dart
@@ -0,0 +1,13 @@
+Map<String, dynamic> allowList = {
+  'cd': true,
+  'ls': true,
+  'mkdir': true,
+  'pwd': true,
+  'rails': {
+    'routes': true,
+    '-h': true,
+    '--help': true,
+    '-v': true,
+    '--version': true,
+  },
+};
